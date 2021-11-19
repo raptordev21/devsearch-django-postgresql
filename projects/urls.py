1fd63463d89dbb3de30a336158f4bad48e67d04b
Himanshu Choudhary <raptordev21@gmail.com>
2021-11-19 14:04:16 +0530

    template and model config

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('projects/', views.projects, name="projects"),
+    path('', views.projects, name="projects"),
     path('project/<str:id>/', views.project, name="project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 
+usersList = [
+    {
+        'id': '1',
+        'name': 'raptor'
+    },
+    {
+        'id': '2',
+        'name': 'ace'
+    },
+    {
+        'id': '3',
+        'name': 'dell'
+    },
+]
+
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    return render(request, 'projects/projects.html', {'users': usersList})
 
 
 def project(request, id):
-    return render(request, 'projects/single-project.html')
+    projectObj = None
+    for i in usersList:
+        if i['id'] == id:
+            projectObj = i
+
+    if projectObj == None:
+        return render(request, 'notfound.html')
+    else:
+        return render(request, 'projects/single-project.html', {'project': projectObj})
